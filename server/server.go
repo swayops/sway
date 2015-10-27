a3779020400202cdfbdd7cb893dca25cdac10f2a
Shahzil Abid <shahzilabid@Shahzils-MBP.attlocal.net>
2015-10-26 23:57:33 -0700

    Add init method

--- server/server.go
+++ server/server.go
@@ -12,8 +12,13 @@ func New(cfg *config.Config) (*Server, error) {
 		Cfg: cfg,
 	}
 	srv.InitializeInfluencers()
+	srv.InitializeCampaigns()
 }
 
 func (srv *Server) InitializeInfluencers() {
+	// Load influencers from DB
+}
 
+func (srv *Server) InitializeCampaigns() {
+	// Load campaigns from DB
 }
