0266aea1e9c219bbf873e3a750990e37004ede63
OneOfOne <oneofone@gmail.com>
2016-11-01 23:15:05 +0200

    brain fart zone..

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pushd $GOPATH/src/github.com/swayops/swayops
+pushd $GOPATH/src/github.com/swayops/sway
 go get -u -ldflags "-X github.com/swayops/sway/server.gitBuild=$(git describe --always --abbrev=16)" || exit 1
 popd
