1c56141af6ea79212641461c50ccd006c4319994
OneOfOne <oneofone@gmail.com>
2016-11-01 22:14:53 +0200

    update sway before the ui repos

--- scripts/update-all.sh
+++ scripts/update-all.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 export NODE_ENV=production
+
+pushd $GOPATH/src/github.com/swayops/sway
+go get -u ./...
+go install -ldflags "-X github.com/swayops/sway/server.gitBuild=$(git describe --always --abbrev=16)" || exit 1
+popd
+
 for repo in $GOPATH/src/github.com/swayops/{dashboard,influencerApp}; do
 	pushd $repo
 	git pull || exit 1
@@ -8,7 +14,3 @@ for repo in $GOPATH/src/github.com/swayops/{dashboard,influencerApp}; do
 	popd
 done
 
-pushd $GOPATH/src/github.com/swayops/sway
-go get -u ./...
-go install -ldflags "-X github.com/swayops/sway/server.gitBuild=$(git describe --always --abbrev=16)" || exit 1
-popd
