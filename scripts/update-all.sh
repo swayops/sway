6fe973be9209301fa64f42648c226000903316ee
OneOfOne <oneofone@gmail.com>
2016-11-02 06:50:51 +0200

    remove the old binary otherwise go get / go install acts stupid on rebuilds

--- scripts/update-all.sh
+++ scripts/update-all.sh
@@ -3,6 +3,7 @@
 export NODE_ENV=production
 
 pushd $GOPATH/src/github.com/swayops/sway
+rm $GOPATH/bin/sway
 go get -u -ldflags "-X github.com/swayops/sway/server.gitBuild=$(git describe --always --abbrev=16)" || exit 1
 popd
 
