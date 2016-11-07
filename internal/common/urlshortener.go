37c4c35639a6c6d9442b2eb12daef64f69629662
OneOfOne <oneofone@gmail.com>
2016-11-08 01:24:57 +0200

    start short ids from 10k and base64 encode it rather than use raw numbers

--- internal/common/urlshortener.go
+++ internal/common/urlshortener.go
@@ -3,21 +3,25 @@ package common
 import (
 	"fmt"
 
+	"encoding/base64"
+
 	"github.com/boltdb/bolt"
 	"github.com/swayops/sway/config"
 	"github.com/swayops/sway/misc"
 )
 
-var format = "%s::%s"
+const format = "%s::%s"
 
 func ShortenID(deal *Deal, tx *bolt.Tx, cfg *config.Config) string {
-	id, err := misc.GetNextIndex(tx, cfg.Bucket.URL)
+	id, err := misc.GetNextIndexBig(tx, cfg.Bucket.URL)
 	if err != nil {
 		return ""
 	}
-	if err := misc.PutBucketBytes(tx, cfg.Bucket.URL, id, []byte(fmt.Sprintf(format, deal.CampaignId, deal.Id))); err != nil {
+	idStr := base64.RawStdEncoding.EncodeToString(id.Bytes())
+
+	if err := misc.PutBucketBytes(tx, cfg.Bucket.URL, idStr, []byte(fmt.Sprintf(format, deal.CampaignId, deal.Id))); err != nil {
 		return ""
 	}
 
-	return id
+	return idStr
 }
