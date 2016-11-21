856b4167358bd14bae69f41824ffad023c5358f4
OneOfOne <oneofone@gmail.com>
2016-11-21 21:07:24 +0200

    clean up and fix with GOPATH

--- scripts/updater.sh
+++ scripts/updater.sh
@@ -24,35 +24,50 @@ function die {
 }
 
 function update-sway() {
-	rm $GOPATH/bin/sway
-	go get -u -ldflags "-X github.com/swayops/sway/server.gitBuild=$(git describe --always --abbrev=16)" || exit 1
+	echo "updating sway"
+	pushd $GOPATH/src/github.com/swayops/sway > /dev/null
+	rm $GOPATH/bin/sway &>/dev/null
+	go get -v -u -ldflags "-X github.com/swayops/sway/server.gitBuild=$(git describe --always --abbrev=16)" || die
+	popd > /dev/null
 }
 
 function update-dash() {
-	pushd $GOPATH/src/github.com/swayops/dashboard || die ""
+	echo "updating dashboard"
+	pushd $GOPATH/src/github.com/swayops/dashboard > /dev/null || die
 	git pull || exit 1
 	npm install || exit 1
 	popd > /dev/null
 }
 
 function update-infApp() {
-	pushd $GOPATH/src/github.com/swayops/influencerApp || die ""
+	echo "updating influencer app"
+	pushd $GOPATH/src/github.com/swayops/influencerApp > /dev/null || die
 	git pull || exit 1
 	npm install || exit 1
 	popd > /dev/null
 }
 
 function update-all() {
+	echo "updating all"
 	update-sway && update-dash && update-infApp
 }
 
 case "$1" in
-	dash|dashboard|update-dash) echo "updating dashboard"; asSwayUser update-dash ;;
-	inf|infApp|influencerApp|update-infApp) echo "updating influencerApp"; asSwayUser update-infApp ;;
-	sway|up|update|update-sway) echo "updating sway"; asSwayUser  update-sway ;;
-	a|all|upa|update-all) echo 'updating all'; asSwayUser update-all;;
-	r|restart) echo 'restarting sway'; sudo 'systemctl restart sway && sleep 2s && systemctl status -l sway';;
-	start) echo 'starting sway'; sudo 'systemctl start sway && sleep 2s && systemctl status -l sway';;
-	stop) echo 'stoping sway'; sudo 'systemctl stop sway';;
-	*) echo "$0 [dash|inf|sway|all]" ;;
+	dash|dashboard|update-dash) asSwayUser update-dash ;;
+
+	inf|infApp|influencerApp|update-infApp) asSwayUser update-infApp ;;
+
+	sway|up|update|update-sway) asSwayUser  update-sway ;;
+
+	a|all|upa|update-all) asSwayUser update-all;;
+
+	r|restart) echo 'restarting sway'; sudo -i /bin/bash -c 'systemctl restart sway && sleep 2s && systemctl status -l sway';;
+
+	start) echo 'starting sway'; sudo -i /bin/bash -c 'systemctl start sway && sleep 2s && systemctl status -l sway';;
+
+	stop) echo 'stoping sway'; sudo -i /bin/bash -c 'systemctl stop sway';;
+
+	st|status) sudo -i /bin/bash -c 'systemctl status -l sway';;
+
+	*) echo "$0 [ dash | inf | sway | all | restart | start | stop | status]" ;;
 esac
