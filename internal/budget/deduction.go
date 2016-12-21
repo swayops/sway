c30c454f8c74fa12fa0851748d239c480123b0a2
Shahzil Abid <shahzilabid@Shahzils-MacBook-Pro.local>
2016-12-21 12:47:44 -0800

    New deductions

--- internal/budget/deduction.go
+++ internal/budget/deduction.go
@@ -2,23 +2,23 @@ package budget
 
 const (
 	// YouTube
-	YT_LIKE    = 0.1
+	YT_LIKE    = 0.2
 	YT_DISLIKE = 0.03
 	YT_VIEW    = 0.003 // $3 CPM
 	YT_COMMENT = 0.35
 
 	// Facebook
-	FB_LIKE    = 0.05
+	FB_LIKE    = 0.15
 	FB_SHARE   = 0.2
-	FB_COMMENT = 0.1
+	FB_COMMENT = 0.2
 
 	// Instagram
-	INSTA_LIKE    = 0.05
-	INSTA_COMMENT = 0.10
+	INSTA_LIKE    = 0.15
+	INSTA_COMMENT = 0.2
 
 	// Twitter
 	TW_RETWEET  = 0.2
-	TW_FAVORITE = 0.05
+	TW_FAVORITE = 0.1
 )
 
 func (store *Store) deductSpendable(val float64) {
