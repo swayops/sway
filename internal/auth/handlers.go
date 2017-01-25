7cb17afc48847dd53e8c8049a71d53d7a3b072fb
OneOfOne <oneofone@gmail.com>
2017-01-26 01:37:08 +0200

    angular doesn't like delete requests with a payload

--- internal/auth/handlers.go
+++ internal/auth/handlers.go
@@ -302,6 +302,7 @@ func (a *Auth) SignUpHandler(c *gin.Context) {
 func (a *Auth) AddSubUserHandler(c *gin.Context) {
 	var (
 		su struct {
+			Name     string `json:"name"`
 			Email    string `json:"email"`
 			Password string `json:"pass"`
 		}
@@ -343,22 +344,15 @@ func (a *Auth) AddSubUserHandler(c *gin.Context) {
 
 func (a *Auth) DelSubUserHandler(c *gin.Context) {
 	var (
-		su struct {
-			Email string `json:"email"`
-		}
-		id = c.Param("id")
+		id    = c.Param("id")
+		email = c.Param("email")
 	)
 
-	if err := c.Bind(&su); err != nil {
-		misc.AbortWithErr(c, http.StatusBadRequest, err)
-		return
-	}
-
 	if err := a.db.Update(func(tx *bolt.Tx) error {
-		if l := a.GetLoginTx(tx, su.Email); l == nil || !l.IsSubUser || l.UserID != id {
+		if l := a.GetLoginTx(tx, email); l == nil || !l.IsSubUser || l.UserID != id {
 			return ErrInvalidRequest
 		}
-		return misc.GetBucket(tx, a.cfg.Bucket.Login).Delete([]byte(su.Email))
+		return misc.GetBucket(tx, a.cfg.Bucket.Login).Delete([]byte(email))
 	}); err != nil {
 		misc.AbortWithErr(c, http.StatusBadRequest, err)
 	} else {
