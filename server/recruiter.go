f83cf53dcdfcafda3047682ee55c1e5a06330d25
Shahzil Abid <shahzilabid@Shahzils-MacBook-Pro.local>
2017-02-02 13:42:42 -0800

    Decrease scraps emails for now

--- server/recruiter.go
+++ server/recruiter.go
@@ -36,8 +36,8 @@ func emailScraps(srv *Server) (int32, error) {
 	now := int32(time.Now().Unix())
 	var count int32
 	for _, sc := range scraps {
-		if count >= 35 {
-			// Only send 20 emails max per run
+		if count >= 5 {
+			// Only send 5 emails max per run
 			break
 		}
 
