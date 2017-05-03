a266041c23ca73f1daedcf881afaff6d8a9187de
Shahzil Abid <shahzilabid@Shahzils-MacBook-Pro.local>
2017-05-03 08:43:20 -0700

    Bump up deal count

--- server/recruiter.go
+++ server/recruiter.go
@@ -30,7 +30,7 @@ func emailScraps(srv *Server) (int32, error) {
 		return 0, ErrWait
 	}
 
-	maxEmails := srv.Campaigns.GetAvailableDealCount()
+	maxEmails := srv.Campaigns.GetAvailableDealCount() * 3
 
 	// Influencers who have signed up
 	signUps := srv.auth.Influencers.GetAllEmails()
