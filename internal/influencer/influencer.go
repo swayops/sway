8f05db4c5cc9a7344e566e6485fde788c7f74ccf
Shahzil Abid <shahzilabid@Shahzils-MacBook-Pro.local>
2017-05-03 13:04:15 -0700

    Get oudda heeyah if ur less than 10k and client wants brand safe

--- internal/influencer/influencer.go
+++ internal/influencer/influencer.go
@@ -983,9 +983,11 @@ func (inf *Influencer) GetAvailableDeals(campaigns *common.Campaigns, audiences 
 
 		// Brand safety check
 		// If the campaign just wants brand safe and the influencer isn't brand safe..
-		if cmp.BrandSafe && inf.BrandSafe != "t" && !query {
-			rejections[cmp.Id] = "BRAND_SAFETY"
-			continue
+		if cmp.BrandSafe && !query {
+			if inf.BrandSafe != "t" || inf.GetFollowers() < 10000 {
+				rejections[cmp.Id] = "BRAND_SAFETY"
+				continue
+			}
 		}
 
 		// Fill in and check available spendable
