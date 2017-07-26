e4828bb1a8b1402521267ab48a1383b2cdc0e92f
Shahzil Abid <shahzilabid@Shahzils-MacBook-Pro.local>
2017-07-26 15:11:05 -0700

    Fuckin bug

--- internal/common/date.go
+++ internal/common/date.go
@@ -51,13 +51,16 @@ func GetDateRangeFromOffsetRange(start, end int) []time.Time {
 	} else if end > 0 {
 		end = -end
 	}
+
 	if end < start {
 		start, end = end, start
 	}
+
 	out := make([]time.Time, (end-start)+1)
 	to := time.Now().UTC().AddDate(0, 0, start)
 	for i := range out {
-		out[i] = to.AddDate(0, 0, end+i)
+		out[i] = to.AddDate(0, 0, i+1)
 	}
+
 	return out
 }
